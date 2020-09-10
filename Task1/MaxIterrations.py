2093f708c4a2e59416cab0b64cdbf0025d09c302
LukichevaPolina <lukichevapolina42@gmail.com>
2020-09-10 19:30:21 +0300

    add dict to MaxIterrations

--- Task1/MaxIterrations.py
+++ Task1/MaxIterrations.py
@@ -1,17 +1,23 @@
 countMaxIterrations = 0
 findNum = 0
-for i in range (1, 1001):
+dictIterrations = {}
+for i in range(1, 1001):
     currentNum = i
     count = 0
-
-    while (currentNum != 1):
+    while currentNum != 1:
+        if currentNum in dictIterrations:
+            dictIterrations[i] = count + dictIterrations[currentNum]
+            break
         if currentNum % 2:
             currentNum = currentNum*3 + 1
         else:
             currentNum /= 2
-        count+=1
-    if (count > countMaxIterrations):
-        countMaxIterrations = count
-        findNum = i
+        count += 1
+    else:
+        dictIterrations[i] = count
 
-print("Number = ", findNum, "\nCount Iterrations = ", countMaxIterrations)
+keys = list(dictIterrations.keys())
+values = list(dictIterrations.values())
+
+print(keys[values.index(max(values))], (max(values)))
+
